d562f79d548d29158bd372e12f771a52b46790e6
Luis Martinez <mrluismartinezzz@gmail.com>
2025-05-03 12:07:29 -0700

    Initial Commit

---
+++ ios101-preworkTests/ios101_preworkTests.swift
@@ -0,0 +1,17 @@
+//
+//  ios101_preworkTests.swift
+//  ios101-preworkTests
+//
+//  Created by million grad on 5/3/25.
+//
+
+import Testing
+@testable import ios101_prework
+
+struct ios101_preworkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
